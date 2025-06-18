d05a218d79cb95f8c84ef2c6d9f3117a995f5890
ferra2391 <ferra2391@gmail.com>
2025-06-18 16:54:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const sheetUrl = "https://docs.google.com/spreadsheets/d/1glvkWLOpUhdBUmY4j-2DJoXQvY6dnQD_OQkYYCaXXEQ/edit?usp=sharing";
+
+fetch(sheetUrl)
+  .then(res => res.json())
+  .then(data => {
+    const presenze = {};
+
+    data.forEach(row => {
+      const nome = row["Nome Operaio"]?.trim();
+      if (nome) {
+        if (!presenze[nome]) presenze[nome] = 0;
+        presenze[nome]++;
+      }
+    });
+
+    const labels = Object.keys(presenze);
+    const valori = Object.values(presenze);
+
+    const ctx = document.getElementById("graficoPresenze").getContext("2d");
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Giorni di presenza",
+          data: valori,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
